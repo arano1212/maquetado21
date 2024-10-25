b5520c6d1741dcdc91082afca4340cb5afeddcbe
arano1212 <arano1212@gmail.com>
2024-10-24 23:03:45 -0600

    feat: first commit in maint-test

--- src/components/ImageBanner/ImageBanner.jsx
+++ src/components/ImageBanner/ImageBanner.jsx
@@ -6,12 +6,12 @@ const ImageBanner = () => {
       style={{
         position: "relative",
         width: "100vw",
-        height: "700px",
+        height: "650px",
         overflow: "hidden",
       }}
     >
       <img
-        src="https://via.placeholder.com/800x400?text=PROVOCA+MIRADAS%2C+QUE+HABLEN+DE+TI"
+        src="https://via.placeholder.com/1200x400?text=image"
         alt="Provoque Miradas"
         style={{
           width: "100%",
@@ -36,7 +36,41 @@ const ImageBanner = () => {
           textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
         }}
       >
-
+        PROVOCA MIRADAS,<br /> QUE HABLEN DE TI
+      </div>
+      {/* Texto corto en la esquina inferior derecha */}
+      <div
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          right: "20px",
+          color: "white",
+          background: "rgba(0, 0, 0, 0.5)", // Fondo semitransparente para legibilidad
+          padding: "10px",
+          borderRadius: "5px",
+        }}
+      >
+        Lorem ipsum dolor sit amet.
+      </div>
+      {/* Iconos en la esquina inferior izquierda */}
+      <div
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          left: "20px",
+          display: "flex",
+          gap: "10px", // Espaciado entre iconos
+        }}
+      >
+        <a href="#" style={{ color: "white", fontSize: "1.5rem" }}>
+          <i className="fab fa-instagram" aria-hidden="true"></i>
+        </a>
+        <a href="#" style={{ color: "white", fontSize: "1.5rem" }}>
+          <i className="fab fa-facebook" aria-hidden="true"></i>
+        </a>
+        <a href="#" style={{ color: "white", fontSize: "1.5rem" }}>
+          <i className="fab fa-linkedin" aria-hidden="true"></i>
+        </a>
       </div>
     </div>
   );
